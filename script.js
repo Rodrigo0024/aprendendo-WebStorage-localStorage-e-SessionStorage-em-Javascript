212529577849c41ad32bc635351cf4d192b1b678
rodrigo <24rodrigocurso@gmail.com>
2025-02-03 20:34:22 -0300

    Aprendi a usar localStorage e sessionStorage

--- script.js
+++ script.js
@@ -6,5 +6,35 @@ const btn_texto = document.querySelector("#btn_texto");
 
 btn_texto.addEventListener("click", (env)=>{
 
-    
+
 });
+
+//Aprendendo como funciona
+
+let num = 10
+
+localStorage.setItem("Numero", num)
+localStorage.setItem("Nome", "mario")
+localStorage.setItem("Numero", 33)
+
+//para obter o valor da chave
+
+alert(localStorage.getItem("Numero"))
+alert(localStorage.getItem("Nome"))
+
+//remove as chaves
+
+//localStorage.clear()
+
+alert(localStorage.key(0)) //mostra oa chave de indice 0
+
+alert(localStorage.getItem(localStorage.key(0)))
+
+//obtendo o tamanho do local storage
+alert(localStorage.length)
+
+//sessionStorage
+
+sessionStorage.setItem("ano", 1997)
+
+//ele tem as mesmas funções só que quando o navegador é fechados os dados somem
