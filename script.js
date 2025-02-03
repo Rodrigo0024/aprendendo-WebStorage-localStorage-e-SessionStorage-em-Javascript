0dc9f4aa8c74a19add472bf048520070a144590e
rodrigo <24rodrigocurso@gmail.com>
2025-02-03 20:05:55 -0300

    Arquivo base js e html preparados

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const texto = document.querySelector("#texto");
+const p_texto = document.querySelector("#p_texto");
+const btn_texto = document.querySelector("#btn_texto");
+
+btn_texto.addEventListener("click", (env)=>{
+
+    
+});
